01018c207cd68c99e37af72a2136f54da3361b66
dongfangxiaoshuo <1563930710@qq.com>
2019-06-26 11:18:23 +0800

    Initial commit

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="counter-warp">
+    
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+    count () {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/me/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div @click="clickHandle">
+
+    
+  </div>
+</template>
+
+<script>
+// import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+   
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
